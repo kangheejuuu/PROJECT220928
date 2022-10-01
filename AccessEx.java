9ad33c443e0b5b8c0ff06f844a2a7ebda4ca5563
강희주 <@LAPTOP-G0D9E46G>
2022-10-01 15:46:32 +0900

    4주차 과제

---
+++ AccessEx.java
@@ -0,0 +1,18 @@
+//2021041025 강희주
+class Sample {
+	public int a;
+	int b;
+	int c;
+}
+
+public class AccessEx {
+
+	public static void main(String[] args) {
+		Sample aClass = new Sample();
+		aClass.a = 10;
+		aClass.b = 10;
+		aClass.c = 10;
+
+	}
+
+}
---
+++ GarbageEx.java
@@ -0,0 +1,16 @@
+//2021041025 강희주
+public class GarbageEx {
+
+	public static void main(String[] args) {
+		String a = new String("Good");
+		String b = new String("Bad");
+		String c = new String("Normal");
+		String d, e;
+		a = null;
+		d = c;
+		c = null;
+
+
+	}
+
+}
